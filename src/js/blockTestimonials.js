39a3e3bc101c6f3bc90e6f71baa5a266299ef5bb
goshkiev <igor.goshkiev@yandex.ru>
2024-12-02 17:43:59 +0300

    testimonial play_1

--- src/js/blockTestimonials.js
+++ src/js/blockTestimonials.js
@@ -36,16 +36,24 @@ export function createBlockTestimonials () {
     class: 'section-testimonials__video-wraper'
   })
 
-  function createCardBox(inFile) {
+  function createCardBox(inFile, textName, textJob) {
 
     const cardBoxWraper = el('div', {
       class: 'section-testimonials__card-box-wraper',
     })
 
+    const cardTextName = el('h3', {
+      class: 'section-testimonials__card-text-name',
+      textContent: textName
+    })
+
+    const cardTextJob = el('p', {
+      class: 'section-testimonials__card-text-job',
+      textContent: textJob
+    })
+
     const customPlay = el('span', {
-      class: 'section-testimonials__playpause',
-      // src: butPlay,
-      // textContent: 'Play'
+      class: 'section-testimonials__playpause section-testimonials__play',
     })
 
     const cardBox = el('video', {
@@ -63,17 +71,19 @@ export function createBlockTestimonials () {
 
     setChildren(cardBoxWraper , [
       cardBox,
+      cardTextName,
+      cardTextJob,
       customPlay
     ])
 
     return cardBoxWraper
   }
 
-  const card1 = createCardBox(inFile1)
-  const card2 = createCardBox(inFile2)
-  const card3 = createCardBox(inFile3)
-  const card4 = createCardBox(inFile4)
-  const card5 = createCardBox(inFile5)
+  const card1 = createCardBox(inFile1, 'Никита Ивановский', 'Директор')
+  const card2 = createCardBox(inFile2, 'Валентина Левицкая', 'Директор')
+  const card3 = createCardBox(inFile3, 'Грисько Евгений', 'Директор')
+  const card4 = createCardBox(inFile4, 'Анна Клочкова', 'Директор')
+  const card5 = createCardBox(inFile5, 'Евгений Грисько', 'Директор')
 
   setChildren(sectionTestimonialsVideo, sectionTestimonialsWraper)
 
